988ec9d8e5e2b9ef0c60ebfe2ea875fc16ee4c0a
duck7x <hgquack@gmail.com>
2023-04-09 15:00:33 -0400

    Documenting general functions

--- general_functions.c
+++ general_functions.c
@@ -1,24 +1,39 @@
 #include "general_functions.h"
 
-/* TODO: Add documentation */
+/*  This function gets a string, representing an error message.
+    The function sets the global variable has_errors to TRUE.
+    The function prints an error message to the user,
+    containing the given message as well as the line number of the error.
+    Printing the line number is done via the line_count global variable.
+*/
 void handle_error(char *error_message) {
     has_errors = TRUE;
     printf("ERROR: %s on line %d\n", error_message, line_count);
 }
 
-/* TODO: Add documentation */
+/*  This function gets a string, representing a warning message.
+    The function prints a warning message to the user,
+    containing the given message as well as the line number of the warning.
+    Printing the line number is done via the line_count global variable.
+*/
 void handle_warning(char *warning_message) {
     printf("WARNING: %s on line %d\n", warning_message, line_count);
 }
 
-/* TODO: Add documentation */
+/*  The function gets two strings.
+    Using strncmp, checks if the first string starts with the second string.
+*/
 int starts_with(char *str, char *prefix) {
     if (strncmp(prefix, str, strlen(prefix)) == 0)
         return TRUE;
     return FALSE;
 }
 
-/* TODO: Add documentation */
+/*  Gets a string and a delimiter character.
+    Using strchr and pointers arithmetics, calculates the index right
+    before the delimiter appears in the string.
+    If the delimiter doesn't appear in the string, returns -1.
+*/
 int get_index_till_delimiter(char* str, char delimiter) {
     char* delimiter_address = strchr(str, delimiter);
     if (delimiter_address) {
@@ -27,7 +42,11 @@ int get_index_till_delimiter(char* str, char delimiter) {
     return -1;
 }
 
-/* TODO: Add documentation */
+/*  Gets a string and a character delimiter.
+    Using get_index_till_delimiter, split the string to a linked list
+    Which contains the string's sub-words as split by the given delimiter.
+    The delimiter will not appear in the list at all.
+*/
 LinkedList_t split_string(char* str, char delimiter) {
     LinkedList_t list;
     int curr_index;
@@ -49,30 +68,29 @@ LinkedList_t split_string(char* str, char delimiter) {
     return list;
 }
 
+/*  Gets a string.
+    Cleans it up by removing any trailing whitespaces at the end and start of it
+    And combining all consecutive whitespaces to a single space character.
+*/
 /* TODO: Ensure that works! */
-/* TODO: Add documentation */
 char* get_clean_and_stripped_string(char* str) {
-    int i;
-    int cleaned_str_index = 0;
-    int following_space = TRUE;
+    int i = 0, cleaned_str_index = 0;
+    int following_space = TRUE;  /* Handles any trailing whitespaces at the start of the string */
 
-    i = 0;
     while (str[i] != END_OF_STRING) {
         if (isSpace(str[i])) {
             if (!is(following_space)) {
                 following_space = TRUE;
-                str[cleaned_str_index] = SPACE;
-                cleaned_str_index++;
+                str[cleaned_str_index++] = SPACE;
             }
         } else {
             following_space = FALSE;
-            str[cleaned_str_index] = str[i];
-            cleaned_str_index++;
+            str[cleaned_str_index++] = str[i];
         }
         i++;
     }
 
-    if (isSpace(str[cleaned_str_index - 1]))
+    if (isSpace(str[cleaned_str_index - 1]))  /* Handles any trailing whitespaces at the end of the string */
         str[cleaned_str_index - 1] = END_OF_STRING;
     else
         str[cleaned_str_index] = END_OF_STRING;
@@ -80,27 +98,21 @@ char* get_clean_and_stripped_string(char* str) {
     return str;
 }
 
-/* TODO: Add documentation */
-/* Replaces all spaces (including tabs!) with a single space! */
+/*  Gets a string.
+    Cleans it up combining all consecutive whitespaces to a single space character.
+*/
 char* clean_multiple_whitespaces(char* str) {
-    int i;
-    /*char* cleaned_str;*/
-    int cleaned_str_index = 0;
-    int following_space = FALSE;
+    int i = 0, cleaned_str_index = 0, following_space = FALSE;
 
-    /*cleaned_str = copy_string(str);*/
-    i = 0;
     while (str[i] != END_OF_STRING) {
         if (isSpace(str[i])) {
             if (!is(following_space)) {
                 following_space = TRUE;
-                str[cleaned_str_index] = SPACE;
-                cleaned_str_index++;
+                str[cleaned_str_index++] = SPACE;
             }
         } else {
             following_space = FALSE;
-            str[cleaned_str_index] = str[i];
-            cleaned_str_index++;
+            str[cleaned_str_index++] = str[i];
         }
         i++;
     }
@@ -111,7 +123,9 @@ char* clean_multiple_whitespaces(char* str) {
 
 }
 
-/* TODO: Add documentation */
+/*  Gets a string.
+    Removes any whitespaces that appear in it.
+*/
 char* get_string_without_whitespaces(char* str) {
     int i;
     int str_len = strlen(str);
@@ -122,15 +136,16 @@ char* get_string_without_whitespaces(char* str) {
 
     for (i=0; i <= str_len; i++) {
         if (!isSpace(str[i])) {
-            cleaned_str[cleaned_str_index] = str[i];
-            cleaned_str_index++;
+            cleaned_str[cleaned_str_index++] = str[i];
         }
     }
 
     return cleaned_str;
 }
 
-/* TODO: Add documentation */
+/*  Gets a string.
+    Cleans it up by removing any trailing whitespaces at the end and start of it.
+*/
 char* get_stripped_string(char* str) {
     int first_char = 0, last_char = strlen(str);
 
@@ -147,7 +162,10 @@ char* get_stripped_string(char* str) {
     return copy_substring(str, first_char, last_char);
 }
 
-/* TODO: Add documentation */
+/* Gets a file and a string.
+   Add that string to the file using fputs.
+   The given file should be in editing mode (write or append).
+*/
 void write_string_to_file(FILE* file, char* string) {
     fputs(string, file);
 }
--- general_functions.h
+++ general_functions.h
@@ -1,36 +1,101 @@
 #ifndef GENERAL_FUNCTIONS_H /* Prevents recursive includes */
 #define GENERAL_FUNCTIONS_H
 
-#include <stdio.h>
 #include "utils.h"
 
-#define NEWLINE '\n' /* TODO: Add documentation */
-#define SPACE ' ' /* TODO: Add documentation */
-#define COMMA ',' /* TODO: Add documentation */
-#define COLON ':' /* TODO: Add documentation */
-#define COLON ':' /* TODO: Add documentation */
-#define COLON ':' /* TODO: Add documentation */
-#define RIGHT_BRACKET ')' /* TODO: Add documentation */
-#define LEFT_BRACKET '(' /* TODO: Add documentation */
-#define READ "r" /* TODO: Add documentation */
-#define WRITE "w" /* TODO: Add documentation */
-#define APPEND "a" /* TODO: Add documentation */
-#define INPUT_SUFFIX ".as" /* TODO: Add documentation */ /* TODO: Maybe move to pre_assembler_functions */
-#define POST_PRE_ASSEMBLER_SUFFIX ".am" /* TODO: Add documentation */
-#define MAX_LINE_LENGTH 80 /* TODO: Add documentation */
-#define ReadLine(FILE, LINE) fscanf(FILE, "%[^\n] ", LINE) /* TODO: Add documentation */
-#define StringsEqual(FIRST, SECOND) strcmp(FIRST, SECOND) == 0 /* TODO: Add documentation */
-#define BITS_AMOUNT 14 /* TODO: Add documentation */ /* TODO: rename this */
+/* Characters in common use*/
+#define NEWLINE '\n'
+#define SPACE ' '
+#define COMMA ','
+#define COLON ':'
+#define RIGHT_BRACKET ')'
+#define LEFT_BRACKET '('
 
-void handle_error(char *error_message); /* TODO: Add documentation */
-void handle_warning(char *warning_message); /* TODO: Add documentation */
-int starts_with(char *str, char *prefix); /* TODO: Add documentation */
-int get_index_till_delimiter(char* str, char delimiter); /* TODO: Add documentation */
-LinkedList_t split_string(char* str, char delimiter); /* TODO: Add documentation */
-char* get_clean_and_stripped_string(char* str); /* TODO: Add documentation */
-char* clean_multiple_whitespaces(char* str); /* TODO: Add documentation */
-char* get_string_without_whitespaces(char* str); /* TODO: Add documentation */
-char* get_stripped_string(char* str); /* TODO: Add documentation */
-void write_string_to_file(FILE* file, char* string); /* TODO: Maybe change name to "append_chunk_to_file */ /* TODO: Add documentation */
+/* File modes */
+#define READ "r"
+#define WRITE "w"
+#define APPEND "a"
+
+#define INPUT_SUFFIX ".as" /* Suffix of the assembler files */
+#define POST_PRE_ASSEMBLER_SUFFIX ".am" /* Suffix of the files created by pre-assembler */
+#define MAX_LINE_LENGTH 80 /* maximum allowed characters in a line */
+#define ReadLine(FILE, LINE) fscanf(FILE, "%[^\n] ", LINE) /* Reading a line from file */
+#define StringsEqual(FIRST, SECOND) strcmp(FIRST, SECOND) == 0 /* Check if wo strings are equal */
+
+/*  Function to handle error messages throughout the assembler phases.
+    Gets an error message, prints it and the line number.
+    Also sets the global variable has_errors to TRUE.
+    INPUT:  error_message is a string representing an error that occurred
+    OUTPUT: This function doesn't return anything.
+*/
+void handle_error(char *error_message);
+
+/*  Function to handle warning messages throughout the assembler phases.
+    Gets a warning message, prints it and the line number.
+    INPUT:  warning_message is a string representing a warning that occurred
+    OUTPUT: This function doesn't return anything.
+*/
+void handle_warning(char *warning_message);
+
+/*  Gets two strings, and checks if the second is the prefix of the first.
+    INPUT:  str is the string we want to check
+            prefix is the string of the prefix we want to check
+    OUTPUT: TRUE if str starts with prefix
+            FALSE if str doesn't start with prefix
+*/
+int starts_with(char *str, char *prefix);
+
+/*  Gets a string and a characters.
+    Returns the index of the character in the string which is right before the given delimiter.
+    INPUT:  str is the string we're searching through.
+            delimiter is the character we're searching for.
+    OUTPUT: An int representing the index in the character.
+            -1 if the character doesn't appear in the string at all.
+*/
+int get_index_till_delimiter(char* str, char delimiter);
+
+/*  Like split method (in python for example),
+    gets a string and splits it into sub-strings according to the given delimiter.
+    INPUT:  str representing a string.
+            delimiter character to split the string by.
+    OUTPUT: a linked list of all substrings of this string, divided by the delimiter (delimiter not included).
+*/
+LinkedList_t split_string(char* str, char delimiter);
+
+/*  Gets a string, returns a clean and stripped version of the string
+    Meaning there are no trailing spaces at the start and end of the returned string
+    All consecutive whitespaces are squeezed together into one space (' ') character.
+    INPUT:  str - the string that should be cleaned.
+    OUTPUT: str - the cleaned string.
+*/
+char* get_clean_and_stripped_string(char* str);
+
+/*  Gets a string.
+    Squeezes all consecutive whitespaces together into one space (' ') character.
+    INPUT:  str - the string that should be cleaned.
+    OUTPUT: str - the cleaned string.
+*/
+char* clean_multiple_whitespaces(char* str);
+
+/*  Gets a string.
+    Returns the string without any whitespaces in it
+    INPUT:  str - the string that should be cleaned.
+    OUTPUT: str - the cleaned string.
+*/
+char* get_string_without_whitespaces(char* str);
+
+/*  Gets a string.
+    Removes any trailing whitespaces at the start or end of the given string.
+    INPUT:  str - the string to clean up.
+    OUTPUT: str - the cleaned up string.
+*/
+char* get_stripped_string(char* str);
+
+/*  Gets a file and a string, writes that string into that file.
+    INPUT:  file - the destination file we want to write to
+            string - the string we want to add to the file
+    OUTPUT: This function doesn't return anything.
+*/
+void write_string_to_file(FILE* file, char* string);
 
 #endif
